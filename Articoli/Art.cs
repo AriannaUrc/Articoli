081623bec8db1f3291e8672409bba74ecdc72447
AriannaUrc <urciuoli.arianna.studente@itispaleocapa.it>
2023-11-16 10:40:16 +0100

    Aggiungere i file di progetto.

---
+++ Articoli/Art.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Articoli
+{
+    internal class Art
+    {
+        protected string _codice;
+        protected string _descrizione;
+        protected float _prezzo;
+
+        public string Codice { get { return _codice; } set {_codice = value; } }
+        public string Descrizione { get { return _descrizione; } set { _descrizione = value; } }
+
+        public float Prezzo { get { return _prezzo; } set { _prezzo = value; } }
+    }
+}
