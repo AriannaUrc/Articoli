0a4ab41405e81416d6629b1ef8b6cecf765f5e7c
AriannaUrc <urciuoli.arianna.studente@itispaleocapa.it>
2023-11-16 10:52:02 +0100

    inizio funzione equals

--- Articoli/Art.cs
+++ Articoli/Art.cs
@@ -15,6 +15,38 @@ namespace Articoli
         public string Codice { get { return _codice; } set {_codice = value; } }
         public string Descrizione { get { return _descrizione; } set { _descrizione = value; } }
 
-        public float Prezzo { get { return _prezzo; } set { _prezzo = value; } }
+        public float Prezzo { get { return _prezzo; } set { if (_prezzo >= 0) { _prezzo = value; } else { _prezzo = 0; } } }
+
+
+        public Art() 
+        {
+            Codice= "";
+            Descrizione = "";
+            Prezzo= 0;
+        }
+
+        public Art(string codice, string des, float prezzo)
+        {
+            Codice = codice;
+            Descrizione = des;
+            Prezzo = prezzo;
+        }
+
+        public bool Equals(Art cmp)
+        {
+            _codice = cmp._codice;
+
+            return true;
+
+        }
+
+
+        public virtual void sconto(bool fedelty)
+        {
+            if(fedelty)
+            {
+                Prezzo -= Prezzo / 100 * 5;
+            }
+        }
     }
 }
