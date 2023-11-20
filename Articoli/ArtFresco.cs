0abd6d26cf864df31ba82c83cc1c47fd0a020893
AriannaUrc <urciuoli.arianna.studente@itispaleocapa.it>
2023-11-20 13:37:10 +0100

    Add sconto ArticoloFresco

---
+++ Articoli/ArtFresco.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Articoli
+{
+    internal class ArtFresco : ArtAlimentari
+    {
+        public ArtFresco() : base() { }
+
+        public ArtFresco(int day, int month, int year, string codice, string des, float prezzo) : base(day, month, year, codice, des, prezzo) { }
+
+        public override void sconto(bool fedelty)
+        {
+            base.sconto(fedelty);
+            if(Scadenza.Year == DateTime.Now.Year && Scadenza.Month == DateTime.Now.Month && Scadenza.Day - DateTime.Now.Day <= 5)
+            {
+                Prezzo -= Prezzo / 100 * (12 - (Scadenza.Day - DateTime.Now.Day) * 2);
+            }
+            
+        }
+    }
+}
