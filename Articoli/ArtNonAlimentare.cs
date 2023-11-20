dee254c0c08cd7d5ba77b0490a3d7f4582579882
AriannaUrc <urciuoli.arianna.studente@itispaleocapa.it>
2023-11-20 13:40:50 +0100

    ArticoloNonAlimentare

---
+++ Articoli/ArtNonAlimentare.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Articoli
+{
+    internal class ArtNonAlimentare : Art
+    {
+        private bool _riciclabile;
+        public bool Riciclabile { get { return _riciclabile; } set { _riciclabile = value; } }
+
+        public ArtNonAlimentare() : base() 
+        {
+            _riciclabile = false;
+        }
+
+        public ArtNonAlimentare(bool ric, string codice, string des, float prezzo) : base(codice, des, prezzo)
+        {
+            Riciclabile = ric;
+        }
+
+        public override void sconto(bool fedelty)
+        {
+            base.sconto(fedelty);
+            if(Riciclabile)
+            {
+                Prezzo -= Prezzo / 100 * 20;
+            }
+
+        }
+
+    }
+}
